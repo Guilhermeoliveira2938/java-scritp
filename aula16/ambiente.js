6e41f4fc4b8aa043378f0026e4d85f6d0e9df3a3
Guilherme <guilhermeoliveira2938@gmail.com>
2026-04-23 17:57:58 -0300

    par ou impar

---
+++ aula16/ambiente.js
@@ -0,0 +1,11 @@
+function parimpar(n) {
+    if (n%2 == 0) {
+        return 'O número é par!'
+    } else {
+        return 'O número é impar!'
+    }
+
+}
+
+let resultado = parimpar(0,21)
+console.log(resultado)
